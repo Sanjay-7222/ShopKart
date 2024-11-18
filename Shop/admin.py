05adceb38db9d2f29487ef0c9af6c4c2ed29bf80
Sanjay-7222 <sanjay8015803208@gmail.com>
2024-11-18 13:22:18 +0530

    add products

--- Shop/admin.py
+++ Shop/admin.py
@@ -6,7 +6,10 @@ from .models import *
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'created_at')
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display =('name', 'category', 'description')
+
 admin.site.register(Category,CategoryAdmin)
-admin.site.register(Product,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Cart)
 admin.site.register(Favourite)
